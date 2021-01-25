c878707727cca90d7e322380fc1e45bd2a7d1e25
Caroline Zago <carolineszago@gmail.com>
2021-01-25 07:53:59 -0300

    [feature] fisrt tests

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 my_web_app = Flask('my_web_app')
 
 @my_web_app.route('/')
 def initial_page():
-    return ''
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    my_web_app.run()
--- tests.py
+++ tests.py
@@ -3,11 +3,25 @@ from app import my_web_app
 
 class TestHome(unittest.TestCase):
 
-    def test_get(self):
+    def setUp(self):
         app = my_web_app.test_client()
-        response = app.get('/')
-        self.assertEqual(200, response.status_code)
-        self.assertIn('text/html', response.content_type)
+        self.response = app.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_content_type(self):
+        self.assertIn('text/html', self.response.content_type)
+    
+    def test_content(self):
+        response_str = self.response.data.decode('utf-8')
+        self.assertIn('<title>Hello World</title>', str(response_str))
+        self.assertIn('<h1>Hello World!</h1>', str(response_str))
+        self.assertIn('<p>Study ', str(response_str))
+    
+    def test_bootstrap_css(self):
+        response_str = self.response.data.decode('utf-8')
+        self.assertIn('bootstrap.min.css', response_str)
 
 if __name__ == '__main__':
     unittest.main()
